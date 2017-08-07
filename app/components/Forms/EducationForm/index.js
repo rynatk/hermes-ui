19189299e3118b3eeaa4a7eda875ae3c09a75627
Tyler Riedal <triedal@mail.usf.edu>
2017-08-06 23:07:28 -0400

    feat(): single education form.

--- app/components/Forms/EducationForm/index.js
+++ app/components/Forms/EducationForm/index.js
@@ -5,12 +5,12 @@
 */
 
 import React from 'react';
+import { Field, reduxForm } from 'redux-form';
 import { Button, Divider, Form } from 'semantic-ui-react';
+import { GenericInput, H1 } from '../components/';
 import PropTypes from 'prop-types';
 
 import Wrapper from '../Wrapper';
-import { H1 } from '../components';
-
 
 class EducationForm extends React.Component { // eslint-disable-line react/prefer-stateless-function
   render() {
@@ -19,8 +19,17 @@ class EducationForm extends React.Component { // eslint-disable-line react/prefe
       <Wrapper>
         <H1>Education</H1>
         <Divider />
-        Build me plz.
         <Form onSubmit={this.props.onSubmit}>
+          <Form.Group>
+            <Field name="education.school" component={GenericInput} label="School" type="text" width={8} />
+            <Field name="education.start-date" component={GenericInput} label="Start Date" type="text" width={4} />
+            <Field name="education.end-date" component={GenericInput} label="End Date" type="text" width={4} />
+          </Form.Group>
+          <Form.Group>
+            <Field name="education.degree" component={GenericInput} label="Degree" type="text" width={8} />
+            <Field name="education.major" component={GenericInput} label="Major" type="text" width={6} />
+            <Field name="education.gpa" component={GenericInput} label="GPA" type="text" width={2} />
+          </Form.Group>
           <Button size="big" type="button" onClick={prevPage}>Previous</Button>
           <Button size="big" type="submit" primary>Next</Button>
         </Form>
@@ -34,4 +43,8 @@ EducationForm.propTypes = {
   prevPage: PropTypes.func,
 };
 
-export default EducationForm;
+export default reduxForm({
+  form: 'application',
+  destroyOnUnmount: false,
+  forceUnregisterOnUnmount: true,
+})(EducationForm);
